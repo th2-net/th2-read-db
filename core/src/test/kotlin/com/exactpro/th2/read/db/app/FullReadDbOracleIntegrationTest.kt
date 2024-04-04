07f227a92d65656030e768a557942b03b5646761
nikita.smirnov <nikita.smirnov@exactprosystems.com>
2024-04-04 12:54:48 +0400

    [TH2-5179] corrected after review

--- core/src/test/kotlin/com/exactpro/th2/read/db/app/FullReadDbOracleIntegrationTest.kt
+++ core/src/test/kotlin/com/exactpro/th2/read/db/app/FullReadDbOracleIntegrationTest.kt
@@ -29,11 +29,11 @@ import com.exactpro.th2.dataprovider.lw.grpc.MessageGroupResponse
 import com.exactpro.th2.dataprovider.lw.grpc.MessageGroupsSearchRequest
 import com.exactpro.th2.dataprovider.lw.grpc.MessageSearchResponse
 import com.exactpro.th2.read.db.annotations.IntegrationTest
+import com.exactpro.th2.read.db.bootstrap.ResourceRegister
 import com.exactpro.th2.read.db.bootstrap.setupApp
 import com.exactpro.th2.read.db.containers.OracleContainer
 import com.exactpro.th2.read.db.grpc.DbPullRequest
 import com.exactpro.th2.read.db.grpc.ReadDbService
-import com.exactpro.th2.read.db.grpc.StopPullingRequest
 import com.exactpro.th2.test.annotations.CustomConfigProvider
 import com.exactpro.th2.test.annotations.Th2AppFactory
 import com.exactpro.th2.test.annotations.Th2IntegrationTest
@@ -160,21 +160,23 @@ internal class FullReadDbOracleIntegrationTest {
                 }
         }, MESSAGE_OUT_PIN)
 
-        setupApp(factory) { name, resource -> resourceCleaner.add(name, resource) }
+
+
         val readDb = test.grpcRouter.getService(ReadDbService::class.java)
+        ResourceRegister().use { register ->
+            setupApp(factory, register::add)
 
-        execute { insertData(listOf(Record(1, Instant.now(), Instant.now(), Instant.now(), Instant.now()))) }
-        var dbPullResponse = readDb.startPulling(dbPullRequest)
-        await("first message").atMost(5, TimeUnit.SECONDS).until { publishedMessages.size == 1 }
-        readDb.stopPulling(StopPullingRequest.newBuilder().setId(dbPullResponse.id).build())
+            execute { insertData(listOf(Record(1, Instant.now(), Instant.now(), Instant.now(), Instant.now()))) }
+            readDb.startPulling(dbPullRequest)
+            await("first message").atMost(5, TimeUnit.SECONDS).until { publishedMessages.size == 1 }
+        }
 
-        Thread.sleep(1)
-
-        execute { insertData(listOf(Record(2, Instant.now(), Instant.now(), Instant.now(), Instant.now()))) }
-        dbPullResponse = readDb.startPulling(dbPullRequest)
-        await("second message").atMost(5, TimeUnit.SECONDS).until { publishedMessages.size == 2 }
-        readDb.stopPulling(StopPullingRequest.newBuilder().setId(dbPullResponse.id).build())
-
+        ResourceRegister().use { register ->
+            setupApp(factory, register::add)
+            execute { insertData(listOf(Record(2, Instant.now(), Instant.now(), Instant.now(), Instant.now()))) }
+            readDb.startPulling(dbPullRequest)
+            await("second message").atMost(5, TimeUnit.SECONDS).until { publishedMessages.size == 2 }
+        }
     }
 
     @Suppress("unused")
